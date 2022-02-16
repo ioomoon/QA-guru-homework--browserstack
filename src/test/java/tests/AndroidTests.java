daa6174c3d5d90bcdde7920561133de3c187600e
ioomoon <lavrushina.maria@mail.ru>
2022-02-16 17:50:19 +0300

    Add test

--- src/test/java/tests/AndroidTests.java
+++ src/test/java/tests/AndroidTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,18 +16,33 @@ public class AndroidTests extends TestBase {
     private WikiSteps steps = new WikiSteps();
 
     @DisplayName("Тест на поиск 'BrowserStack'")
+    @Feature("WikiSearch")
+    @Owner("m.lavrushina")
     @Test
     void searchTest() throws MalformedURLException, InterruptedException {
         steps.searchWikiClick();
-        steps.sendKeysBrowserStack();
+        steps.sendKeys("BrowserStack");
         steps.assertResultsExist();
     }
 
     @DisplayName("Тест на проверку языка поиска")
+    @Feature("WikiSearch")
+    @Owner("m.lavrushina")
     @Test
     void checkLanguage() throws MalformedURLException, InterruptedException {
         steps.searchWikiClick();
-        steps.sendKeysBrowserStack();
+        steps.sendKeys("BrowserStack");
         steps.assertLanguageIsEN();
+    }
+
+    @Test
+    @DisplayName("Открытие страницы из результатов поиска")
+    @Feature("WikiSearch")
+    @Owner("m.lavrushina")
+    void openingPageFromSearchResultsTest() {
+        steps.searchWikiClick();
+        steps.sendKeys("Java");
+        steps.clickResult("Java");
+        steps.checkSubtitleTextContains("Indonesian island");
     }
 }
--- src/test/java/tests/steps/WikiSteps.java
+++ src/test/java/tests/steps/WikiSteps.java
@@ -5,6 +5,7 @@ import io.appium.java_client.MobileBy;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,8 +16,8 @@ public class WikiSteps {
     }
 
     @Step("Ввести значение 'BrowserStack'")
-    public void sendKeysBrowserStack() {
-        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+    public void sendKeys(String key) {
+        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys(key);
     }
 
     @Step("Проверить отображение результатов поиска")
@@ -26,7 +27,17 @@ public class WikiSteps {
 
     @Step("Проверить, что язык поиска - Английский")
     public void assertLanguageIsEN() {
-        $$(byClassName("android.widget.TextView")).first().shouldHave(Condition.text("EN"));
+        $$(byClassName("android.widget.TextView")).first().shouldHave(text("EN"));
+    }
+
+    @Step("Нажать на результат поиска")
+    public void clickResult(String result){
+        $$(byClassName("android.widget.TextView")).find(text(result)).click();
+    }
+
+    @Step("Проверить отображение подзаголовка на открывшейся странице")
+    public void checkSubtitleTextContains(String text){
+        $(MobileBy.id("org.wikipedia.alpha:id/view_page_subtitle_text")).shouldHave(text(text));
     }
 
 }
